804cacad35b1c1ae35b29a20f9be2d88219e63a5
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2026-08-26 14:53:36 +0700

    Add files via upload

--- immintrin.h
+++ immintrin.h
@@ -1,6 +1,24 @@
 #pragma once
 #define __mm256 float*
 
+/*@
+predicate zeroed(float* A, integer b, integer e) = \forall integer i; b <= i < e ==> A[i] == 0;
+
+inductive zeroed_ind(float* a, integer b, integer e){
+case zeroed_empty:
+    \forall float* a, integer b, e; b >= e ==> zeroed_ind(a, b, e);
+case zeroed_range:
+    \forall float* a, integer b, e; b < e ==>
+    zeroed_ind(a, b, e-1) && a[e-1] == 0 ==> zeroed_ind(a,b,e);
+}
+
+lemma zeroed_lin_to_ind:
+    \forall float *a, integer b, integer e; zeroed(a, b, e) ==> zeroed_ind(a, b, e);
+
+lemma zeroed_ind_to_lin:
+    \forall float *a, integer b, integer e; zeroed_ind(a, b, e) ==> zeroed(a, b, e);
+*/
+
 __mm256 REG1; // __mm256_setzero_ps
 __mm256 REG2; // __mm256_set1_ps
 __mm256 REG3; // __mm256_loadu_ps
@@ -12,74 +30,77 @@ int COUNTER3 = 0; // __mm256_loadu_ps
 int COUNTER4 = 0; // __mm256_fmadd_ps
 
 /*@
-    requires \valid(REG1 + (COUNTER1 * 8 .. COUNTER1 * 8 + 7));
-    requires COUNTER1 >= 0;
+requires \valid(REG1 + (COUNTER1 * 8 .. COUNTER1 * 8 + 7));
+requires COUNTER1 >= 0;
+assigns REG1[COUNTER1 * 8 .. COUNTER1 * 8 + 7], COUNTER1;
+ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == 0.0;
+ensures \result == REG1 + COUNTER1 * 8;
+ensures COUNTER1 == 1 + \old(COUNTER1);
+ensures \valid(\result + (0 .. 7));
 
-    ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == 0.0;
-    ensures \result == REG1 + COUNTER1 * 8;
-    ensures COUNTER1 == 1 + \old(COUNTER1);
-
-    assigns REG1[COUNTER1 * 8 + 0 .. COUNTER1 * 8 + 7], COUNTER1;
-    assigns \result \from REG1, COUNTER1;
+assigns \result \from \nothing;
 */
-__mm256 _mm256_setzero_ps(void);
+__mm256 _mm256_setzero_ps();
 
 /*@
-    requires \valid(REG2 + (COUNTER2 * 8 .. COUNTER2 * 8 + 7));
-    requires COUNTER2 >= 0;
-
-    ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == v;
-    ensures \result == REG2 + COUNTER2 * 8;
-    ensures COUNTER2 == 1 + \old(COUNTER2);
-
-    assigns REG2[COUNTER2 * 8 + 0 .. COUNTER2 * 8 + 7];
-    assigns \result \from REG2, COUNTER2, v;
+requires \valid(REG2 + (COUNTER2 * 8 .. COUNTER2 * 8 + 7));
+requires COUNTER2 >= 0;
+assigns REG2[COUNTER2 * 8 .. COUNTER2 * 8 + 7];
+ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == v;
+ensures \result == REG2 + COUNTER2 * 8;
+ensures COUNTER2 == 1 + \old(COUNTER2);
+ensures \valid(\result + (0 .. 7));
 */
 __mm256 _mm256_set1_ps(float v);
 
 /*@
-    requires \valid(REG3 + (COUNTER3 * 8 .. COUNTER3 * 8 + 7));
-    requires \valid_read(a + (0 .. 7));
-    requires COUNTER3 >= 0;
-    requires \separated(a + (0 .. 7), REG3 + (COUNTER3 * 8 .. COUNTER3 * 8 + 7));
-
-    ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == a[i];
-    ensures \result == REG3 + COUNTER3 * 8;
-    ensures COUNTER3 == 1 + \old(COUNTER3);
-
-    assigns REG3[COUNTER3 * 8 + 0 .. COUNTER3 * 8 + 7];
-    assigns \result \from REG3, COUNTER3, a;
+requires \valid(REG3 + (COUNTER3 * 8 .. COUNTER3 * 8 + 7));
+requires \valid_read(a + (0 .. 7));
+requires COUNTER3 >= 0;
+requires \separated(a + (0 .. 7), REG3 + (COUNTER3 * 8 .. COUNTER3 * 8 + 7));
+assigns REG3[COUNTER3 * 8 .. COUNTER3 * 8 + 7];
+ensures \forall integer i; 0 <= i <= 7 ==> \result[i] == a[i];
+ensures \result == REG3 + COUNTER3 * 8;
+ensures COUNTER3 == 1 + \old(COUNTER3);
+ensures \valid(\result + (0 .. 7));
 */
 __mm256 _mm256_loadu_ps(float* a);
 
+/*@
+requires \valid(mem_addr + (0 .. 7));
+
+ensures zeroed(mem_addr, 0, 8);
+ensures \valid(mem_addr + (0 .. 7));
+
+assigns mem_addr[0 .. 7];
+*/
+void setZero(float *mem_addr);
+
 
 /*@
-    requires \valid(mem_addr + (0 .. 7));
-    requires \valid_read(a + (0 .. 7));
-    requires \separated(a + (0 .. 7), mem_addr + (0 .. 7));
-
-    ensures \forall integer i; 0 <= i <= 7 ==> mem_addr[i] == a[i];
-
-    assigns mem_addr[0 .. 7];
+requires \valid(mem_addr + (0 .. 7));
+requires \valid_read(a + (0 .. 7));
+assigns mem_addr[0 .. 7];
+ensures \forall integer i; 0 <= i <= 7 ==> mem_addr[i] == a[i];
+ensures \valid(mem_addr + (0 .. 7));
+ensures mem_addr == \old(mem_addr);
 */
 void _mm256_storeu_ps(float * mem_addr, __mm256 a);
 
 
 /*@
-    requires \valid_read(a + (0 .. 7));
-    requires \valid_read(b + (0 .. 7));
-    requires \valid_read(c + (0 .. 7));
-    requires \valid(REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
-    requires \separated(a + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
-    requires \separated(b + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
-    requires \separated(c + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
-    requires COUNTER4 >= 0;
-
-    ensures \forall integer i; 0 <= i < 8 ==> \result[i] == a[i] * b[i] + c[i];
-    ensures \result == REG4 + COUNTER4 * 8;
-    ensures COUNTER4 == 1 + \old(COUNTER4);
-
-    assigns REG4[COUNTER4 * 8 + 0 .. COUNTER4 * 8 + 7];
-    assigns \result \from REG4, COUNTER4, a, b, c;
+requires \valid_read(a + (0 .. 7));
+requires \valid_read(b + (0 .. 7));
+requires \valid_read(c + (0 .. 7));
+requires \valid(REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
+requires \separated(a + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
+requires \separated(b + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
+requires \separated(c + (0 .. 7), REG4 + (COUNTER4 * 8 .. COUNTER4 * 8 + 7));
+requires COUNTER4 >= 0;
+assigns REG4[COUNTER4 * 8 .. COUNTER4 * 8 + 7];
+ensures \forall integer i; 0 <= i < 8 ==> \result[i] == a[i] * b[i] + c[i];
+ensures \result == REG4 + COUNTER4 * 8;
+ensures COUNTER4 == 1 + \old(COUNTER4);
+ensures \valid(\result + (0 .. 7));
 */
 __mm256 _mm256_fmadd_ps(__mm256 a, __mm256 b, __mm256 c);
--- main.c
+++ main.c
@@ -1,39 +1,30 @@
 #include "immintrin.h"
 
 /*@
-  axiomatic MatrixMult {
-    
-  inductive zeroed{L}(float* a, integer b, integer e) {
-    case zeroed_empty{L}:
-        \forall float* a, integer b, e; b >= e ==> zeroed{L}(a, b, e);
-
-    case zeroed_range{L}:
-        \forall float* a, integer b, e; b < e ==>
-        zeroed{L}(a, b, e-1) && a[e-1] == 0 ==> zeroed{L}(a,b,e);
-    }
-    
+  axiomatic MatrixMult
+  {
     inductive DotProduction(float* A, float* B, integer to, 
-                                   integer i, integer K, integer j, integer N, real res) {
+                                   integer i, integer K, integer j, integer N, real res)
+	{
     case dotproduction_empty_range:
-        \forall float* A, float* B, integer to, integer i, integer K, integer j, integer N;
-        0 >= to 
-        ==> DotProduction(A, B, to, i, K, j, N, 0.0);
+      \forall float* A, float* B, integer to, integer i, integer K, integer j, integer N;
+      0 >= to ==> DotProduction(A, B, to, i, K, j, N, 0.0);
 
     case dotproduction_positive_range:
       \forall float* A, float* B, integer to, integer i, integer K, integer j, integer N, real res;
       (0 < to) &&
-      DotProduction(A, B, to-1, i, K, j, N, res) 
-      ==> DotProduction(A, B, to, i, K, j, N, res + A[i*K + (to-1)] * B[(to-1)*N + j]);
-	}
-    
+      DotProduction(A, B, to-1, i, K, j, N, res) ==> 
+      DotProduction(A, B, to, i, K, j, N, res + A[i*K + (to-1)] * B[(to-1)*N + j]);
+      
+	}      
     inductive RowResult{L2}(float* A, float* B, float* C, integer to, 
-                                integer i, integer K, integer N) {
-    case rowresult_empty{L2}:
+                                integer i, integer K, integer N) 
+    {
+      case rowresult_empty{L2}:
         \forall float* A, float* B, float* C, integer to, integer i, integer K, integer N; 
-        0 >= to 
-        ==> RowResult{L2}(A, B, C, to, i, K, N);
+        0 >= to ==> RowResult{L2}(A, B, C, to, i, K, N);
 
-    case rowresult_step{L2}:
+      case rowresult_step{L2}:
         \forall float* A, float* B, float* C, integer to, integer i, integer K, integer N; 
         0 < to &&
         RowResult{L2}(A, B, C, to-1, i, K, N) && 
@@ -42,102 +33,227 @@
     }
        
     inductive MatrixResult{L2}(float* A, float* B, float* C, integer to, 
-                                   integer K, integer N, integer M) {
-    case matrixresult_empty{L2}:
+                                   integer K, integer N, integer M) 
+    {
+      case matrixresult_empty{L2}:
         \forall float* A, float* B, float* C, integer to, integer K, integer N, integer M; 
         0 >= to ==> MatrixResult{L2}(A, B, C, to, K, N, M);
 
-    case matrixresult_step{L2}:
+      case matrixresult_step{L2}:
         \forall float* A, float* B, float* C, integer to, integer K, integer N, integer M;
         0 < to &&
         MatrixResult{L2}(A, B, C, to-1, K, N, M) && 
         RowResult{L2}(A, B, C, N, to-1, K, N)
         ==> MatrixResult{L2}(A, B, C, to, K, N, M);
     }
-}
+  }
+*/
 
+/*@
+lemma zeroed_contat:
+  \forall float *A, integer b, integer c, integer d; b < c < d && zeroed(A, b, c) && zeroed(A, c, d) ==> zeroed(A, b, d);
+
+lemma zeroed_succ:
+  \forall float *A, integer b, integer e; b < e && zeroed(A, b, e) && A[e] == 0 ==> zeroed(A, b, e+1);
+
+lemma zeroed_last:
+  \forall float *A, integer b, integer e; b < e+1 && zeroed(A, b, e+1) ==> A[e] == 0;
+
+lemma zeroed_first:
+  \forall float *A, integer b, integer e; b < e && zeroed(A, b, e) ==> A[b] == 0;
+
+lemma eq_index:
+  \forall float *A, integer j, integer k; A + k + j == A + j + k;
+
+lemma eq_cells:
+  \forall float *A, integer shift, integer b; (A + shift)[b] == A[shift + b];
+
+lemma zeroed_shift_0:
+  \forall float *A, integer b, integer e, integer shift; shift == 0 && zeroed(A+shift, b, e) 
+    ==> zeroed(A, b+shift, e+shift);
+
+lemma eq_cells_all:
+  \forall float *A, integer b, integer e, integer i; b <= i < e ==> (A + b)[i] == A[b + i];
+
+lemma shiftAAAAAA: 
+  \forall float *A, integer b, integer e, integer shift, integer i; b <= i < e && (A + shift)[i] == 0 
+    ==> A[i + shift] == 0;
+
+lemma zeroed_shift_comm:
+  \forall float *A, integer shift, integer b, integer e; shift >= 0 && 0 <= b < e && zeroed(A+shift, b, e) 
+  ==> zeroed(A, b+shift, e+shift);
+
+lemma zeroed_shift:
+  \forall float *A, integer shift, integer b, integer e; shift >= 0 && 0 <= b < e && zeroed(A+shift, b, e) 
+  ==> zeroed(A, shift+b, shift+e);
+
+lemma zeroed_shift_comm_inv:
+  \forall float *A, integer shift, integer b, integer e; shift >= 0 && 0 <= b < e && zeroed(A, b+shift, e+shift) 
+  ==> zeroed(A+shift, b, e);
+
+lemma zeroed_shift_inv:
+  \forall float *A, integer shift, integer b, integer e; shift >= 0 && 0 <= b < e && zeroed(A, shift+b, shift+e)
+  ==> zeroed(A+shift, b, e);
+
+lemma zeroed_0_8:
+  \forall float *c, integer j; j >= 0 && zeroed(c+j, 0, 8)
+  ==> zeroed(c, j+0, j+8);
+*/
+
+/*@
   lemma math_bounds:
     \forall integer i, M, N;
     0 <= i < M && 0 < N ==> 0 <= i*N && i*N + N <= M*N;
 
   lemma div8:
     \forall integer m; m % 16 == 0 ==> m % 8 == 0;
-*/
 
+*/
 
 /*@
-    requires COUNTER1 == 0;
-    requires COUNTER2 == 0;
-    requires COUNTER3 == 0;
-    requires COUNTER4 == 0;
-    requires \valid_read(A + (0 .. M*K-1));
-    requires \valid_read(B + (0 .. K*N-1));
-    requires \valid(C + (0 .. M*N-1));
-    requires \forall integer i; \valid(REG1 + (i * 8 .. i * 8 + 7));
-    requires \forall integer i; \valid(REG2 + (i * 8 .. i * 8 + 7));
-    requires \forall integer i; \valid(REG3 + (i * 8 .. i * 8 + 7));
-    requires \forall integer i; \valid(REG4 + (i * 8 .. i * 8 + 7));
-    requires \forall integer i, j, k, l; \separated(REG1 + (i * 8 .. i * 8 + 7), REG2 + (j * 8 .. j * 8 + 7), REG3 + (k * 8 .. k * 8 + 7), REG4 + (l * 8 .. l * 8 + 7), A + (0 .. M*K-1), C + (0 .. M*N-1));
-    requires \forall integer i, j, k, l; \separated(REG1 + (i * 8 .. i * 8 + 7), REG2 + (j * 8 .. j * 8 + 7), REG3 + (k * 8 .. k * 8 + 7), REG4 + (l * 8 .. l * 8 + 7), B + (0 .. K*N-1), C + (0 .. M*N-1));
-    requires M > 0 && N > 0 && K > 0;
-    requires N % 16 == 0;
+  requires COUNTER1 == 0;
+  requires COUNTER2 == 0;
+  requires COUNTER3 == 0;
+  requires COUNTER4 == 0;
+  requires \valid_read(A + (0 .. M*K-1));
+  requires \valid_read(B + (0 .. K*N-1));
+  requires \valid(C + (0 .. M*N-1));
+  requires \forall integer i; \valid(REG1 + (i * 8 .. i * 8 + 7));
+  requires \forall integer i; \valid(REG2 + (i * 8 .. i * 8 + 7));
+  requires \forall integer i; \valid(REG3 + (i * 8 .. i * 8 + 7));
+  requires \forall integer i; \valid(REG4 + (i * 8 .. i * 8 + 7));
+  requires \forall integer i, j, k, l; \separated(REG1 + (i * 8 .. i * 8 + 7), REG2 + (j * 8 .. j * 8 + 7), REG3 + (k * 8 .. k * 8 + 7), REG4 + (l * 8 .. l * 8 + 7), A + (0 .. M*K-1), C + (0 .. M*N-1));
+  requires \forall integer i, j, k, l; \separated(REG1 + (i * 8 .. i * 8 + 7), REG2 + (j * 8 .. j * 8 + 7), REG3 + (k * 8 .. k * 8 + 7), REG4 + (l * 8 .. l * 8 + 7), B + (0 .. K*N-1), C + (0 .. M*N-1));
+  requires M > 0 && N > 0 && K > 0;
+  requires N % 16 == 0;
 
-    ensures MatrixResult{Post}(A, B, C, M, K, N, M);
+  ensures MatrixResult{Post}(A, B, C, M, K, N, M);
 */
-void gemm_v2(int M, int N, int K, const float * A, const float * B, float * C) {
+void gemm_v2(int M, int N, int K, const float * A, const float * B, float * C)
+{
     /*@
-        loop invariant 0 <= i <= M;
-        loop invariant MatrixResult{Here}(A, B, C, i, K, N, M);
-        loop invariant COUNTER1 >= 0;
-        loop invariant COUNTER2 >= 0;
-
-        loop assigns C[0 .. M*N-1], i, REG1[0 .. COUNTER1 * 8 + 7], REG2[0 .. COUNTER2 * 8 + 7], REG3[0 .. COUNTER3 * 8 + 7], REG4[0 .. COUNTER4 * 8 + 7], COUNTER1, COUNTER2, COUNTER3, COUNTER4;
-        loop variant M-i;
+      loop invariant \at(N, Pre) == N;
+      loop invariant \at(M, Pre) == M;
+      loop invariant \at(K, Pre) == K;
+      loop invariant 0 <= i <= M;
+      loop invariant N % 16 == 0;
+      loop invariant MatrixResult{Here}(A, B, C, i, K, N, M);
+      loop invariant COUNTER1 >= 0;
+      loop invariant COUNTER2 >= 0;
+      loop invariant \valid(C + (0 .. M*N-1));
+      
+      loop variant M-i;
     */
-    for (int i = 0; i < M; ++i) {
+    for (int i = 0; i < M; ++i)
+    {
         float * c = C + i * N;
-
         /*@
-            loop invariant 0 <= j <= N;
-            loop invariant j % 8 == 0;
-            loop invariant 0 <= i < M;
-            loop invariant zeroed{Here}(C, i*N, i*N + j);
-            loop invariant COUNTER1 >= 0;
-            
-            loop assigns j, C[i*N .. i*N + N - 1], REG1[0 .. COUNTER1 * 8 + 7], COUNTER1; 
-            loop variant N-j;
+          loop invariant \at(N, Pre) == N;
+          loop invariant \at(M, Pre) == M;
+          loop invariant \at(K, Pre) == K;
+          loop invariant N % 16 == 0;
+          loop invariant 0 <= j <= N;
+          loop invariant j % 8 == 0;
+          loop invariant 0 <= i < M;
+          loop invariant zeroed(C, i*N, i*N + j);
+          loop invariant COUNTER1 >= 0;
+          loop invariant \valid(C + (0 .. M*N-1));
+          loop invariant c == C + i*N;
+
+          loop variant N-j;
         */
-        for (int j = 0; j < N; j += 8) {
-            _mm256_storeu_ps(c + j + 0, _mm256_setzero_ps());
-            /*@ assert zeroed{Here}(C, i*N, i*N + j + 8);*/
+        for (int j = 0; j < N; j += 8)
+        {
+          /*@ assert \valid(c + j + 0 + (0 .. 7)); */
+          /*@ assert zeroed(C, i*N, i*N + j); */
+          /*@ assert C[i*N] == 0; */
+
+          // __mm256 tmp = _mm256_setzero_ps();
+          /* assert C[i*N] == 0; */
+          /* assert zeroed(C, i*N, i*N + j); */
+          // _mm256_storeu_ps(c + j + 0, tmp);
+
+          setZero(c + j);
+          /*@ assert zeroed(c+j, 0, 8); */
+          int q = 7;
+          /*@ assert zeroed(c, j+0, j+8); */
+          /*@ assert *(c + j) == 0; */
+          /*@ assert c == C + i*N; */
+          /*@ assert zeroed(C, i*N + j, i*N + j + 8); */
+
+          /*@ assert zeroed(C, i*N, i*N + j+1); */
+          /*@ assert zeroed(C + i*N, 0, j+1); */
+
+          /*@ assert C[i*N + j + 0] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 1); */
+
+          /*@ assert C[i*N + j + 1] == 0; */
+          /*@ assert i*N < i*N+j+1 && zeroed(C, i*N, i*N + j + 1) && C[i*N+j+1] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 2); */
+
+          /*@ assert C[i*N + j + 2] == 0; */
+          /*@ assert i*N < i*N+j+2 && zeroed(C, i*N, i*N + j + 2) && C[i*N+j+2] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 3); */
+
+          /*@ assert C[i*N + j + 3] == 0; */
+          /*@ assert i*N < i*N+j+2 && zeroed(C, i*N, i*N + j + 2) && C[i*N+j+2] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 3); */
+
+          /*@ assert C[i*N + j + 3] == 0; */
+          /*@ assert i*N < i*N+j+3 && zeroed(C, i*N, i*N + j + 3) && C[i*N+j+3] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 4); */
+
+          /*@ assert C[i*N + j + 4] == 0; */
+          /*@ assert i*N < i*N+j+4 && zeroed(C, i*N, i*N + j + 4) && C[i*N+j+4] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 5); */
+
+          /*@ assert C[i*N + j + 5] == 0; */
+          /*@ assert i*N < i*N+j+5 && zeroed(C, i*N, i*N + j + 5) && C[i*N+j+5] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 6); */
+
+          /*@ assert C[i*N + j + 6] == 0; */
+          /*@ assert i*N < i*N+j+6 && zeroed(C, i*N, i*N + j + 6) && C[i*N+j+6] == 0; */
+          /*@ assert zeroed(C, i*N, i*N + j + 7); */
+
+          /*@ assert C[i*N + j + 7] == 0; */
+          /*@ assert i*N < i*N+j+7 && zeroed(C, i*N, i*N + j + 7) && C[i*N+j+7] == 0; */
+          /* assert zeroed(C, i*N, i*N + j + 8); */
         }
-        /*@ assert zeroed{Here}(C, i*N, i*N + N);*/ 
+        /*@ assert zeroed(C, i*N, i*N + N);*/ 
+
 
         /*@
+            loop invariant \at(N, Pre) == N;
+            loop invariant \at(M, Pre) == M;
+            loop invariant \at(K, Pre) == K;
             loop invariant 0 <= k <= K;
             loop invariant 0 <= i < M;
-			loop invariant \forall integer l; 0<=l<N ==> DotProduction(A, B, k, i, K, l, N, c[l]);
+			      loop invariant \forall integer l; 0<=l<N ==> DotProduction(A, B, k, i, K, l, N, c[l]);
             loop invariant COUNTER2 >= 0;
+            loop invariant \valid(C + (0 .. M*N-1));
 
-            loop assigns k, C[i*N .. i*N + N - 1], REG2[0 .. COUNTER2 * 8 + 7], REG3[0 .. COUNTER3 * 8 + 7], REG4[0 .. COUNTER4 * 8 + 7], COUNTER2, COUNTER3, COUNTER4;
             loop variant K-k;
         */
-        for (int k = 0; k < K; ++k) {
+        for (int k = 0; k < K; ++k)
+        {
             const float * b = B + k * N;
             __mm256 a = _mm256_set1_ps(A[i*K + k]);
-
             /*@
+                loop invariant \at(N, Pre) == N;
+                loop invariant \at(M, Pre) == M;
+                loop invariant \at(K, Pre) == K;
                 loop invariant 0 <= j <= N;
                 loop invariant 0 <= k < K;
                 loop invariant 0 <= i < M;
                 loop invariant j % 16 == 0;
                 loop invariant \forall integer q; 0 <= q < j ==> DotProduction(A, B, k+1, i, K, q, N, c[q]);
                 loop invariant \forall integer q; j <= q < N ==> DotProduction(A, B, k, i, K, q, N, c[q]);
+                loop invariant \valid(C + (0 .. M*N-1));
 
-                loop assigns C[i*N .. i*N + N - 1], j, REG2[0 .. COUNTER2 * 8 + 7], REG3[0 .. COUNTER3 * 8 + 7], REG4[0 .. COUNTER4 * 8 + 7], COUNTER2, COUNTER3, COUNTER4;
                 loop variant N-j;
             */
-            for (int j = 0; j < N; j += 16) {
+            for (int j = 0; j < N; j += 16)
+            {
                /*@ assert step0: DotProduction(A, B, k+1, i, K, j, N, c[j]+a[0]*b[j]); */
                /*@ assert step1: DotProduction(A, B, k+1, i, K, j+1, N, c[j+1]+a[1]*b[j+1]); */
                /*@ assert step2: DotProduction(A, B, k+1, i, K, j+2, N, c[j+2]+a[2]*b[j+2]); */
@@ -147,7 +263,7 @@ void gemm_v2(int M, int N, int K, const float * A, const float * B, float * C) {
                /*@ assert step6: DotProduction(A, B, k+1, i, K, j+6, N, c[j+6]+a[6]*b[j+6]); */
                /*@ assert step7: DotProduction(A, B, k+1, i, K, j+7, N, c[j+7]+a[7]*b[j+7]); */
                 _mm256_storeu_ps(c + j + 0, _mm256_fmadd_ps(a, 
-                    _mm256_loadu_ps((float*)(b + j + 0)), _mm256_loadu_ps(c + j + 0)));
+                    _mm256_loadu_ps(b + j + 0), _mm256_loadu_ps(c + j + 0)));
                /*@ assert step8: DotProduction(A, B, k+1, i, K, j+8, N, c[j+8]+a[0]*b[j+8]); */
                /*@ assert step9: DotProduction(A, B, k+1, i, K, j+9, N, c[j+9]+a[1]*b[j+9]); */
                /*@ assert step10: DotProduction(A, B, k+1, i, K, j+10, N, c[j+10]+a[2]*b[j+10]); */
@@ -157,11 +273,13 @@ void gemm_v2(int M, int N, int K, const float * A, const float * B, float * C) {
                /*@ assert step14: DotProduction(A, B, k+1, i, K, j+14, N, c[j+14]+a[6]*b[j+14]); */
                /*@ assert step15: DotProduction(A, B, k+1, i, K, j+15, N, c[j+15]+a[7]*b[j+15]); */
                 _mm256_storeu_ps(c + j + 8, _mm256_fmadd_ps(a, 
-                    _mm256_loadu_ps((float*)(b + j + 8)), _mm256_loadu_ps(c + j + 8)));
+                    _mm256_loadu_ps(b + j + 8), _mm256_loadu_ps(c + j + 8)));
             }
             /*@ assert dot_done:  \forall integer l; 0<=l<N ==> DotProduction(A, B, k+1, i, K, l, N, c[l]);*/
         }
         /*@ assert row_done: RowResult{Here}(A, B, C, N, i, K, N); */
         /*@ assert mat_step: MatrixResult{Here}(A, B, C, i+1, K, N, M); */
+        /*@ assert \at(M, Pre) == M; */
+        /*@ assert \at(K, Pre) == K; */
     }
 }
